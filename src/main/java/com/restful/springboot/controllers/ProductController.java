30a226238ea26d54c19702f06033f463565c3994
Bruno Moura Soares <brunomourasoares@gmail.com>
2023-06-08 13:21:12 -0300

    findAll and FindById

--- src/main/java/com/restful/springboot/controllers/ProductController.java
+++ src/main/java/com/restful/springboot/controllers/ProductController.java
@@ -1,16 +1,51 @@
 package com.restful.springboot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restful.springboot.dtos.ProductRecordDto;
+import com.restful.springboot.models.ProductModel;
 import com.restful.springboot.repositories.ProductRepository;
 
+import jakarta.validation.Valid;
+
+
 @RestController
-@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+    
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> createProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public Object getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    }
     
 }
